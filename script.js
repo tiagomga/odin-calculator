6f06959803f4744b374ee2f3d541941bd12d3a70
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-15 22:47:56 +0000

    Add event to handle operators

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const operation = [];
 
+const operators = {
+    addition: '+',
+    subtraction: '-',
+    multiplication: '*',
+    division: '/',
+    equals: '=',
+};
+
 // Event to add numbers to calculator's display
 document.addEventListener('click', (event) => {
+    const display = document.querySelector('.display');
     if ('number' === event.target.classList.value) {
         let number = event.target.textContent;
-        document.querySelector('.display').textContent += number;
+        display.textContent += number;
         operation.push(number);
+    } else if ('operator' === event.target.classList.value) {
+        display.textContent = '';
+        operation.push(operators[event.target.id]);
     } else if ('ac' === event.target.id) {
-        document.querySelector('.display').textContent = '';
+        display.textContent = '';
         while (operation.length !== 0) {
             operation.pop();
         }
     } else if ('back' === event.target.id) {
-        document.querySelector('.display').textContent = document.querySelector('.display').textContent.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
         operation.pop();
     }
 })
