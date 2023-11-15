df54a502e41b285de89fb5676134596ff3938e4f
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-15 22:50:00 +0000

    Add multiply() function

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ function subtract(a, b) {
     return a - b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
 // Event to add numbers to calculator's display
 document.addEventListener('click', (event) => {
     const display = document.querySelector('.display');
