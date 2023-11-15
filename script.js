0e53282f489f2838d016b67ae64dce518e9b3800
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-15 21:27:12 +0000

    Register pressed numbers in array

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ document.addEventListener('click', (event) => {
     if ('number' === event.target.classList.value) {
         let number = event.target.textContent;
         document.querySelector('.display').textContent += number;
+        operation.push(number);
     }
 })
 
 document.addEventListener('click', (event) => {
     if ('ac' === event.target.id) {
         document.querySelector('.display').textContent = '';
+        while (operation.length !== 0) {
+            operation.pop();
+        }
     }
 })
