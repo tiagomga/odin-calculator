5fade7000b733e0b4d8f8557dba4bcc71f9b61d3
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-17 00:12:25 +0000

    Start over - Operator functions already functioning

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const operation = [];
-
-const operators = {
-    addition: ['/+', add],
-    subtraction: ['-', subtract],
-    multiplication: ['*', multiply],
-    division: ['/', divide],
-    equals: '=',
-};
-
-let usedOperator = false;
-
 function add(a, b) {
     return a + b;
 }
@@ -26,20 +14,6 @@ function divide(a, b) {
     return a / b;
 }
 
-function performOperation() {
-    let operationString = operation.join('');
-    for (let operator in operators) {
-        operatorSymbol = operators[operator][0];
-        operatorFunction = operators[operator][1];
-        if (operationString.search(operatorSymbol) !== -1) {
-            expression = operationString.split(operatorSymbol);
-            a = expression[0];
-            b = expression[1];
-            return operatorFunction(a, b);
-        }
-    }
-}
-
 // Event to add numbers to calculator's display
 document.addEventListener('click', (event) => {
     const display = document.querySelector('.display');
@@ -48,16 +22,11 @@ document.addEventListener('click', (event) => {
         display.textContent += number;
         operation.push(number);
     } else if ('operator' === event.target.classList.value) {
-        display.textContent = '';
-        operation.push(operators[event.target.id][0]);
-        usedOperator = true;
+            display.textContent = '';
     } else if ('ac' === event.target.id) {
         display.textContent = '';
-        while (operation.length !== 0) {
-            operation.pop();
-        }
     } else if ('back' === event.target.id) {
         display.textContent = display.textContent.slice(0, -1);
         operation.pop();
     }
-})
+});
