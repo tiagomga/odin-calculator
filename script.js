c652becd6fa80e6128e1426fe3db74c8c34ef2b7
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-22 00:44:59 +0000

    Add functionality to the calculator

--- script.js
+++ script.js
@@ -1,30 +1,103 @@
-function add(a, b) {
+function addition(a, b) {
     return a + b;
 }
 
-function subtract(a, b) {
+function subtraction(a, b) {
     return a - b;
 }
 
-function multiply(a, b) {
+function multiplication(a, b) {
     return a * b;
 }
 
-function divide(a, b) {
+function division(a, b) {
     return a / b;
 }
 
-// Event to handle numbers in calculator's display
+const operation = {
+    addition: addition,
+    subtraction: subtraction,
+    multiplication: multiplication,
+    division: division,
+}
+
+let reset = false;
+
+let a = -1;
+let operator = null;
+let b = -1;
+
 document.addEventListener('click', (event) => {
     const display = document.querySelector('.display');
     if ('number' === event.target.classList.value) {
-        let number = event.target.textContent;
-        display.textContent += number;
+        // Clear display
+        if (reset) {
+            display.textContent = '';
+            reset = false;
+        }
+
+        // Clear display after 'a' is set and operator is selected
+        if (a !== -1 && b === -1) {
+            display.textContent = '';
+            let number = event.target.textContent;
+            display.textContent += number;
+            b = display.textContent;
+            
+        // Enter numbers in the display
+        } else {
+            let number = event.target.textContent;
+            display.textContent += number;
+        }
+
     } else if ('operator' === event.target.classList.value) {
-        display.textContent = '';
+        // Set 'a' and then 'b' after 'a' is set
+        if (a === -1) {
+            a = display.textContent;
+        } else {
+            b = display.textContent;
+        }
+
+        // Do nothing when 'equals' is pressed when no operator is selected
+        if (event.target.id === 'equals' && operator === null) {
+            display.textContent = a;
+            a = -1;
+            b = -1;
+            reset = true;
+
+        // Do operation when 'equals' is pressed and operator is selected
+        } else if (event.target.id === 'equals') {
+            result = operator(+a, +b);
+            display.textContent = result;
+            a = -1;
+            operator = null;
+            b = -1;
+            reset = true;
+
+        // Select operator
+        } else if (operator === null || b === -1) {
+            operator = operation[event.target.id];
+
+        // Do operation with current operator and change operator if
+        // selected operator is different from the current operator
+        } else {
+            result = operator(+a, +b);
+            display.textContent = result;
+            a = result;
+            operator = operation[event.target.id];
+            b = -1;
+        }
+
+    // Reset calculator to clean state
     } else if ('ac' === event.target.id) {
         display.textContent = '';
+        a = -1;
+        operator = null;
+        b = -1;
+
+    // If no operator is selected, delete one number when 'back' is pressed
     } else if ('back' === event.target.id) {
-        display.textContent = display.textContent.slice(0, -1);
+        if (!operator) {
+            display.textContent = display.textContent.slice(0, -1);
+        }
     }
 });
